2cbc88e130697ee53fbe7cea194198ed067b4d1c
chentianhao <chentianhao@360.net>
2018-09-30 11:46:37 +0800

    初始化springcloudserver版本

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import java.nio.charset.Charset;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -45,5 +47,4 @@ public class WebConfig{
 		};
 	}
 
-	
 }
